adc9a0a5b356c05361a02f1e1c584e42d04d36a5
curoli <curoli@gmail.com>
2022-04-29 17:26:28 -0400

    Slight refactoring.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-use crate::config::{get_config, Config};
+use crate::config::{Config, get_config};
 use crate::error::Error;
-use crate::read::read_vcf;
 
 mod config;
 mod error;
@@ -8,33 +7,11 @@ mod sim;
 mod read;
 mod locus;
 mod phenotype;
+mod vcf;
 
 pub fn run() -> Result<(), Error> {
     let config = get_config()?;
     match config {
-        Config::Vcf(vcf_config) => {
-            let mut inputs_iter = vcf_config.input.iter();
-            match inputs_iter.next() {
-                None => {
-                    return Err(Error::from("Need to specify at least one input file."));
-                }
-                Some(input) => {
-                    println!("Loading phenotypes from {}", vcf_config.phenotype_file);
-                    let phenotypes =
-                        phenotype::load::load(&vcf_config.phenotype_file)?;
-                    println!("Next reading {}", input);
-                    let mut sim_all = read_vcf(input, &phenotypes)?;
-                    println!("File: {}", sim_all.create_summary());
-                    for input in inputs_iter {
-                        println!("Next reading {}", input);
-                        let sim_input = read_vcf(input, &phenotypes)?;
-                        println!("File: {}", sim_input.create_summary());
-                        sim_all = sim_all.try_add(&sim_input)?;
-                        println!("All : {}", sim_all.create_summary());
-                    }
-                }
-            }
-            Ok(())
-        }
+        Config::Vcf(vcf_config) => { vcf::process_vcf(&vcf_config) }
     }
 }
---
+++ src/vcf.rs
@@ -0,0 +1,29 @@
+use crate::config::VcfConfig;
+use crate::error::Error;
+use crate::phenotype;
+use crate::read::read_vcf;
+
+pub(crate) fn process_vcf(vcf_config: &VcfConfig) -> Result<(), Error> {
+    let mut inputs_iter = vcf_config.input.iter();
+    match inputs_iter.next() {
+        None => {
+            return Err(Error::from("Need to specify at least one input file."));
+        }
+        Some(input) => {
+            println!("Loading phenotypes from {}", vcf_config.phenotype_file);
+            let phenotypes =
+                phenotype::load::load(&vcf_config.phenotype_file)?;
+            println!("Next reading {}", input);
+            let mut sim_all = read_vcf(input, &phenotypes)?;
+            println!("File: {}", sim_all.create_summary());
+            for input in inputs_iter {
+                println!("Next reading {}", input);
+                let sim_input = read_vcf(input, &phenotypes)?;
+                println!("File: {}", sim_input.create_summary());
+                sim_all = sim_all.try_add(&sim_input)?;
+                println!("All : {}", sim_all.create_summary());
+            }
+        }
+    }
+    Ok(())
+}
