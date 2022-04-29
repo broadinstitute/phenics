4dbf977fd40f87a573221522ff7445aafa3b6a75
curoli <curoli@gmail.com>
2022-04-29 17:10:48 -0400

    Introducing subcommands.

--- src/config.rs
+++ src/config.rs
@@ -1,39 +1,59 @@
-use clap::{command, Arg};
+use clap::{command, Arg, Command};
 use crate::error::Error;
 use crate::error;
 
-pub(crate) struct Config {
-    pub(crate) inputs: Vec<String>,
-    pub(crate) phenotype_file: String
+pub(crate) enum Config {
+    Vcf(VcfConfig)
+}
+
+pub(crate) struct VcfConfig {
+    pub(crate) input: Vec<String>,
+    pub(crate) phenotype_file: String,
 }
 
 pub(crate) fn get_config() -> Result<Config, Error> {
+    const VCF: &str = "vcf";
     const INPUT: &str = "input";
     const PHENOTYPE: &str = "phenotype";
     let app = command!()
-        .arg_required_else_help(true)
-        .arg(Arg::new(INPUT)
-            .short('i')
-            .long(INPUT)
-            .takes_value(true)
-            .value_name("FILE")
-            .multiple_values(true)
-            .help("Input files (VCF)")
-        )
-        .arg(Arg::new(PHENOTYPE)
-            .short('p')
-            .long(PHENOTYPE)
-            .takes_value(true)
-            .value_name("FILE")
-            .help("Phenotype definitions file")
+        .subcommand(
+            Command::new(VCF)
+                .arg_required_else_help(true)
+                .arg(Arg::new(INPUT)
+                    .short('i')
+                    .long(INPUT)
+                    .takes_value(true)
+                    .value_name("FILE")
+                    .multiple_values(true)
+                    .help("Input files (VCF)")
+                )
+                .arg(Arg::new(PHENOTYPE)
+                    .short('p')
+                    .long(PHENOTYPE)
+                    .takes_value(true)
+                    .value_name("FILE")
+                    .help("Phenotype definitions file")
+                )
         );
-    let matches = app.try_get_matches()?;
-    let inputs =
-        error::none_to_error(matches.values_of(INPUT),
-                             "Need to specify input files")?
-            .map(String::from).collect();
-    let phenotype_file =
-        String::from(error::none_to_error(matches.value_of(PHENOTYPE),
-                                              "Need to specify phenotype definitions")?);
-    Ok(Config { inputs, phenotype_file })
+    let arg_matches = app.try_get_matches()?;
+    match arg_matches.subcommand() {
+        Some((VCF, vcf_matches)) => {
+            let inputs =
+                error::none_to_error(vcf_matches.values_of(INPUT),
+                                     "Need to specify input files")?
+                    .map(String::from).collect();
+            let phenotype_file =
+                String::from(error::none_to_error(vcf_matches.value_of(PHENOTYPE),
+                                                  "Need to specify phenotype definitions")?);
+            Ok(Config::Vcf(VcfConfig { input: inputs, phenotype_file }))
+        }
+        Some(match_with_sub) => {
+            let subcommand_name = match_with_sub.0;
+            Err(Error::from(format!("Unknown subcommand {}. Available is only {}.",
+                                    subcommand_name, VCF)))
+        }
+        None => {
+            Err(Error::from(format!("Missing. Available is only {}.", VCF)))
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use crate::config::get_config;
+use crate::config::{get_config, Config};
 use crate::error::Error;
 use crate::read::read_vcf;
 
@@ -11,25 +11,30 @@ mod phenotype;
 
 pub fn run() -> Result<(), Error> {
     let config = get_config()?;
-    let mut inputs_iter = config.inputs.iter();
-    match inputs_iter.next() {
-        None => {
-            return Err(Error::from("Need to specify at least one input file."));
-        }
-        Some(input) => {
-            println!("Loading phenotypes from {}", config.phenotype_file);
-            let phenotypes = phenotype::load::load(&config.phenotype_file)?;
-            println!("Next reading {}", input);
-            let mut sim_all = read_vcf(input, &phenotypes)?;
-            println!("File: {}", sim_all.create_summary());
-            for input in inputs_iter {
-                println!("Next reading {}", input);
-                let sim_input = read_vcf(input, &phenotypes)?;
-                println!("File: {}", sim_input.create_summary());
-                sim_all = sim_all.try_add(&sim_input)?;
-                println!("All : {}", sim_all.create_summary());
+    match config {
+        Config::Vcf(vcf_config) => {
+            let mut inputs_iter = vcf_config.input.iter();
+            match inputs_iter.next() {
+                None => {
+                    return Err(Error::from("Need to specify at least one input file."));
+                }
+                Some(input) => {
+                    println!("Loading phenotypes from {}", vcf_config.phenotype_file);
+                    let phenotypes =
+                        phenotype::load::load(&vcf_config.phenotype_file)?;
+                    println!("Next reading {}", input);
+                    let mut sim_all = read_vcf(input, &phenotypes)?;
+                    println!("File: {}", sim_all.create_summary());
+                    for input in inputs_iter {
+                        println!("Next reading {}", input);
+                        let sim_input = read_vcf(input, &phenotypes)?;
+                        println!("File: {}", sim_input.create_summary());
+                        sim_all = sim_all.try_add(&sim_input)?;
+                        println!("All : {}", sim_all.create_summary());
+                    }
+                }
             }
+            Ok(())
         }
     }
-    Ok(())
 }
