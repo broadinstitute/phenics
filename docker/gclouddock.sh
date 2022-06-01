8ae8f6bf634421833b92a8a25d837ae7047b45e3
curoli <curoli@gmail.com>
2022-06-01 15:30:54 -0400

    Next attempt to fix OpenSSL issue. Advancing version to 0.2.7.

--- docker/gclouddock.sh
+++ docker/gclouddock.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 project="nitrogenase-docker"
 name="phenics"
-tag="0.2.6"
+tag="0.2.7"
 full="${name}:${tag}"
 echo "Using Google project ${project}, Docker project ${name}, full tag ${full}"
 echo "Cloud-building Docker image:"
