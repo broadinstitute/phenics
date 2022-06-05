699e049956ae9222fd9adee2ce720829c2d3c631
curoli <curoli@gmail.com>
2022-06-05 17:13:09 -0400

    Possibly fix empty bytes edge case. Advancing version to 0.2.28.

--- docker/gclouddock.sh
+++ docker/gclouddock.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 project="nitrogenase-docker"
 name="phenics"
-tag="0.2.27"
+tag="0.2.28"
 full="${name}:${tag}"
 echo "Using Google project ${project}, Docker project ${name}, full tag ${full}"
 echo "Cloud-building Docker image:"
--- docker/testshell.sh
+++ docker/testshell.sh
@@ -1,1 +1,1 @@
-sudo docker run -it gcr.io/nitrogenase-docker/phenics:0.2.27 bash
+sudo docker run -it gcr.io/nitrogenase-docker/phenics:0.2.28 bash
