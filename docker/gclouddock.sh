6697a1281810b746a5a121c36fe02d0fc645be39
curoli <curoli@gmail.com>
2022-06-09 13:45:52 -0400

    Upgrading noodles from 0.23.0 to 0.24.0; Advancing version to 0.2.47.

--- docker/gclouddock.sh
+++ docker/gclouddock.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 project="nitrogenase-docker"
 name="phenics"
-tag="0.2.46"
+tag="0.2.47"
 full="${name}:${tag}"
 echo "Using Google project ${project}, Docker project ${name}, full tag ${full}"
 echo "Cloud-building Docker image:"
